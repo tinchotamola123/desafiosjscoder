729a8c4b0c6de3a9bc9c345bb273b46a42d96c07
mtamola <tinchotamola123@gmail.com>
2022-07-11 19:51:59 -0300

    primera subida

---
+++ Desafio1/js/index.js
@@ -0,0 +1,32 @@
+/*
+// Ejemplo 1:
+
+let numero1 = 0;
+let resultado = 0;
+for(let i=0 ; i<5 ; i++){
+    numero1=parseInt(prompt("Ingrese un numero: "));
+    
+    resultado = numero1 + 10;
+    alert(resultado);
+}
+
+//Ejemplo 2:
+
+let texto1 = "";
+let resultado2 ="El texto ingresado es: ";
+texto1 = prompt("Ingrese un texto (para cancelar ESC): ")
+while (texto1 != "ESC") {
+    resultado2 = resultado2 + texto1;
+    console.log(resultado2);
+    texto1 = prompt("Ingrese otro texto: ")
+}
+*/
+//Ejemplo 3:
+
+let numero2 = 0;
+let resultado3="";
+numero2 = parseInt(prompt("Ingresa la cantidad de veces que quiere ver HOLA: "));
+console.log(numero2);
+for(let i=1 ; i<=numero2 ; i++){   
+    console.log(`${i} Hola`);    
+}
