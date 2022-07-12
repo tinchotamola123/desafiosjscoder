73fa81e341dd7b8e0df0d9f7be70ee8c443b9a39
mtamola <tinchotamola123@gmail.com>
2022-07-12 13:54:02 -0300

    subida ejercicios

--- Desafio1/js/index.js
+++ Desafio1/js/index.js
@@ -43,3 +43,22 @@ if (edad>=18) {
 }
 
 //Ejemplo 5:
+
+let nota=0;
+let resultado5="";
+
+nota = parseInt(prompt("Ingrese la nota obtenida: "));
+
+if (nota>=0 && nota <= 3) {
+    alert("Muy deficiente");
+} else if (nota>3 && nota <= 5) {
+    alert("Insuficiente");
+}else if (nota>5 && nota <= 6) {
+    alert("Suficiente");
+}else if (nota>6 && nota <= 7) {
+    alert("Bien");
+}else if (nota>7 && nota <= 9) {
+    alert("Notable");
+}else if (nota>9 && nota <= 10) {
+    alert("Sobresaliente");
+}
